b9e0f7da89465ebc7aa7d4c1fd94114ef671e52c
chahla1 <cbouchot@myuax.com>
2023-11-28 11:54:43 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -5,4 +5,13 @@
 #include "json11.hpp"
 #include "jsonlib.hpp"
 
+// Enumeración para los tipos de datos admitidos
+enum Variant_Type{Symbol, Number, List, Proc, Lambda, Cadena};
+
+// Estructura para representar un entorno
+struct Entorno;
+
+
+
+
 
